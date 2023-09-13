913e398dd953837cf2f9826ecc3bed61c9412b12
saurav j <saurav.saurav12@gmail.com>
2023-09-13 21:43:38 +0530

    Python Implementation of bubble sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,24 +1,24 @@
 # Write a program to implement bubble sort
-unordered_list = [30, 20, 45, 17, 38, 78]
+unordered_list = [6,5,3,1,8,7,2,4]
 
 # Method-1
-for i in range(len(unordered_list) - 1):
-    for j in range(len(unordered_list) - 1):
+for i in range(len(unordered_list)):
+    for j in range(i):
         if unordered_list[j] > unordered_list[j + 1]:
             unordered_list[j], unordered_list[j + 1] = unordered_list[j + 1], unordered_list[j]
 print(unordered_list)
 
 
 # Method-2
-# def bubble_sort(arr: list) -> list:
-#     swapped = False
-#     for i in range(len(arr) - 1):
-#         for j in range(len(arr) - i - 1):
-#             if arr[j] > arr[j + 1]:
-#                 swapped = True
-#                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-#             if not swapped:
-#                 return
-#     return arr
+def bubble_sort(arr: list) -> list:
+    swapped = False
+    for i in range(len(arr)):
+        for j in range(len(arr)-1):
+            if arr[j] > arr[j + 1]:
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            if not swapped:
+                return
+    return arr
 
-# print(bubble_sort(unordered_list))
+print(bubble_sort(unordered_list))
